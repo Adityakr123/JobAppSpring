5094b11d7811168fac69e533c4fba21b005d4bd4
Aditya <adityakr365@gmail.com>
2025-04-13 13:19:01 +0530

    first job api

---
+++ src/main/java/com/aditya/firstjobapp/jobs/controller/jobController.java
@@ -0,0 +1,18 @@
+package com.aditya.firstjobapp.jobs.controller;
+
+import com.aditya.firstjobapp.jobs.model.Job;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class jobController {
+    private List<Job> jobs = new ArrayList<>();
+
+    @GetMapping("/jobs")
+    public List<Job> findAll(){
+        return jobs;
+    }
+}
